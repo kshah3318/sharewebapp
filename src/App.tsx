fb88bfd2d4eb487b61bac6ff92dfdb5ed77fe301
Sakib Memon <memonsakib1495@gmail.com>
2024-05-30 17:12:27 +0530

    Add react router doms

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider, RouteObject } from 'react-router-dom';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const routes: RouteObject[] = [
+  {
+    path: '/signin',
+    element: <SignIn />
+  },
+  {
+    path: '/signup',
+    element: <SignUp />
+  },
+  {
+    path: '*',
+    element: <SignIn /> // Redirect to SignIn for any undefined routes
+  }
+];
+
+const App = () => {
+  const router = createBrowserRouter(routes);
+  return  <RouterProvider router={router} />;
 }
+
 
 export default App;
---
+++ src/components/SignIn.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const SignIn = () => {
+
+    return (
+        <h1>This is sign in</h1>
+    )
+}
+
+export default SignIn;
